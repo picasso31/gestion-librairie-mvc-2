24165b9bc1b213f919d41571f71044dc4f45e54d
benaaicha <benaichamohammed@outlook.fr>
2024-01-16 16:37:55 +0100

    commit 17h00

--- Views/view_all_livres.php
+++ Views/view_all_livres.php
@@ -1,32 +1,39 @@
 <div>
     <p> <?= isset($search)?'Recherche par '.$search:'' ?></p>
-<table id='table'>
-    <thead>
-        <th>ISBN</th>
-        <th>Titre</th>
-        <th>Theme</th>
-        <th>Nombre de pages</th>
-        <th>Format</th>
-        <th>Nom de l'auteur</th>
-        <th>Prénom</th>
-        <th>Éditeur</th>
-        <th>Année d'édition</th>
-        <th>Prix</th>
-        <th>Langue</th>
+
+
+
+        <table id='table' style="border:3px solid black">
+    <thead style ="background-color:gray;">
+       <th style="border:3px solid black"> <strong> &nbsp; ISBN&nbsp;</strong></th> 
+        <strog><th style="border:3px solid black">  <strong> &nbsp; Titre&nbsp; <strong> </th>
+        <strog><th style="border:3px solid black"> <strong> &nbsp;Theme&nbsp; <strong></th>
+        <strog><th style="border:3px solid black">  <strong> &nbsp; Nombre de pages&nbsp; <strong></th>
+        <strog><th style="border:3px solid black"> <strong>&nbsp; Format&nbsp;<strong></th>
+        <strog><th style="border:3px solid black"> <strong>&nbsp;Nom de l'auteur&nbsp; <strong></th>
+        <strog><th style="border:3px solid black"> <strong> &nbsp; Prénom&nbsp; <strong></th>
+        <strog><th style="border:3px solid black">  <strong>&nbsp;Éditeur&nbsp;<strong></th>
+        <strog><th style="border:3px solid black"> <strong>&nbsp; Année d'édition&nbsp; <strong></th>
+        <strog><th style="border:3px solid black"><strong> &nbsp;Prix &nbsp; <strong></th>
+        <strog><th style="border:3px solid black"> <strong>&nbsp;Langue &nbsp;<strong></th>
+
+
+
     </thead>
     <?php  foreach($livre as $l ): ?>
     <tr>
-        <td><?=$l->ISBN?></td>
-        <td><?=$l->Titre_livre?></td>
-        <td><?=$l->Theme_livre?></td>
-        <td><?=$l->Nbr_pages_livre?></td>
-        <td><?=$l->Format_livre?></td>
-        <td><?=$l->Nom_auteur?></td>
-        <td><?=$l->Prenom_auteur?></td>
-        <td><?=$l->Editeur?></td>
-        <td><?=$l->Annee_edition?></td>
-        <td><?=$l->Prix_vente?></td>
-        <td><?=$l->Langue_livre?></td>
+         <tr style="border:3px solid black">
+        <td style="border:3px solid black">&nbsp;<?=$l->ISBN?>&nbsp;</td>
+        <td style="border:3px solid black">&nbsp;<?=$l->Titre_livre?>&nbsp;</td>
+        <td style="border:3px solid black">&nbsp;<?=$l->Theme_livre?>&nbsp;</td>
+        <td style="border:3px solid black">&nbsp;<?=$l->Nbr_pages_livre?>&nbsp;</td>
+        <td style="border:3px solid black">&nbsp;<?=$l->Format_livre?>&nbsp;</td>
+        <td style="border:3px solid black">&nbsp;<?=$l->Nom_auteur?>&nbsp;</td>
+        <td style="border:3px solid black">&nbsp;<?=$l->Prenom_auteur?>&nbsp;</td>
+        <td style="border:3px solid black">&nbsp;<?=$l->Editeur?>&nbsp;</td>
+        <td style="border:3px solid black">&nbsp;<?=$l->Annee_edition?>&nbsp;</td>
+        <td style="border:3px solid black">&nbsp;<?=$l->Prix_vente?>&nbsp;</td>
+        <td style="border:3px solid black">&nbsp;<?=$l->Langue_livre?>&nbsp;</td>
        
     </tr>
     <?php endforeach; ?>
